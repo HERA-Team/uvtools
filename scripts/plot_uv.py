fc49c0fb7324d19ad975373ca1b842d15a825877
Aaron Parsons <aparsons@berkeley.edu>
2019-06-21 16:57:28 -0700

    Added time and frequency indexing to plot_uv.py

--- scripts/plot_uv.py
+++ scripts/plot_uv.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python
 
 """
-Creates waterfall plots for visibility datasets.  Can tile multiple plots
-on one window, or plot just a single baseline.  When taking the delay
-transform (-d), channels are interepreted as selecting delays after the
-the transform operation.  Similarly, the time select (-t) will be interpreted
-as selecting fringe rates if the fringe-rate transform (-f) has been selected.
-In both cases, the ranges specified are intepreted to be the units of the
-output plot (i.e. as specified by --chan_axis and --time_axis).
+Creates waterfall plots for visibility datasets.
 """
 
 from __future__ import print_function, division, absolute_import
 
-import aipy as a, numpy as np, sys, optparse
+import numpy as np, sys, optparse
 import uvtools
 import pyuvdata
 import hera_cal
 from matplotlib import pylab as plt
 
-# XXX TODO: support chan and time indexing
-
 o = optparse.OptionParser()
 o.set_usage('plot_uv.py [options] *.uv')
 o.set_description(__doc__)
-a.scripting.add_standard_options(o, ant=True, chan=True,
-    cmap=True, max=True, drng=True)
+o.add_option('-a', '--ant', dest='ant', default='auto',
+    help='Select ant_pol/baselines to include.  Examples: auto (of active baselines, only i=j; default), or specific ant/pol pairings (3x_4x,5x_6y).')
+o.add_option('-c', '--chan', dest='chan', default='all',
+    help='Select channels to plot.  Examples: all (all channels), 0_10 (channels from 0 to 10, including 0 and 10) 0_10_2 (channels from 0 to 10, counting by 2), 0,10,20_30 (mix of individual channels and ranges).  Default is "all".')
+o.add_option('--cmap', dest='cmap', default='coolwarm',
+    help='Colormap for plotting.  Default is coolwarm.')
+o.add_option( '--max',dest='max',type='float',default=None,
+    help='Manually set the maximum color level, in units matching plotting mode. Default max(data).')
+o.add_option('--drng', dest='drng', type='float', default=None,
+    help="Dynamic range in color of image, in units matching plotting mode. Default max(data)-min(data).")
 o.add_option('-m', '--mode', dest='mode', default='log',
-    help='Plot mode can be log (logrithmic), lin (linear), phs (phase), real, or imag.')
-#o.add_option('-t', '--time', dest='time', default='all', help='Select which time sample to plot. Options are: "all" (default), "<time1 #>_<time2 #>" (a range of times to plot), or "<time1 #>,<time2 #>" (a list of times to plot). If "all" or a range are selected, a 2-d image will be plotted. If a list of times is selected an xy plot will be generated.')
+    help='Plot mode can be log (logrithmic), abs (absolute), phs (phase), real, or imag.')
+o.add_option('-t', '--time', dest='time', default='all', 
+    help='Select which time samples to plot. Options are: "all" (default), "<time1 #>_<time2 #>" (a range of times to plot), or "<time1 #>,<time2 #>" (a list of times to plot).')
 o.add_option('-u', '--unmask', dest='unmask', action='store_true',
     help='Plot masked data, too.')
 o.add_option('-o', '--out_file', dest='out_file', default='',
@@ -35,7 +36,7 @@ o.add_option('-o', '--out_file', dest='out_file', default='',
 o.add_option('--time_axis', dest='time_axis', default='jd',
     help='Choose time axis to be integration index (cnt), julian date (jd) or local sidereal time (lst).  Default is jd.')
 o.add_option('--freq_axis', dest='freq_axis', default='freq',
-    help='Choose specral axis to be channel index (chan) or frequency (freq).  Default is freq.')
+    help='Choose spectral axis to be channel index (chan) or frequency (freq).  Default is freq.')
 o.add_option('--nolegend', dest='nolegend', action='store_true',
     help='Omit legend in last plot.')
 o.add_option('--share', dest='share', action='store_true',
@@ -59,49 +60,16 @@ XLABELS = {'chan': 'Frequency (channel)',
 }
 
 def parse_ants(antstr):
+    """Split apart command-line antennas into a list of baselines."""
     rv = [s.split('_') for s in antstr.split(',')]
     rv = [(int(i[:-1]), int(j[:-1]), i[-1]+j[-1]) for i,j in rv]
     return rv
 
-def convert_arg_range(arg):
+def parse_range(chanstr):
     """Split apart command-line lists/ranges into a list of numbers."""
-    arg = arg.split(',')
-    return [map(float, option.split('_')) for option in arg]
-
-def gen_times(timeopt, uv, coords, decimate):
-    if timeopt == 'all':
-        def time_selector(t, cnt): return True
-    else:
-        timeopt = convert_arg_range(timeopt)
-        if len(timeopt[0]) != 1:
-            def time_selector(t, cnt):
-                if coords == 'index': t = cnt
-                for opt in timeopt:
-                    if (t >= opt[0]) and (t < opt[1]): return True
-                return False
-        else:
-            timeopt = [opt[0] for opt in timeopt]
-            inttime = uv['inttime'] / a.const.s_per_day * decimate
-            def time_selector(t, cnt):
-                if coords == 'index': return cnt in timeopt
-                for opt in timeopt:
-                    if (t >= opt) and (t < opt + inttime): return True
-                return False
-    return time_selector
-
-def data_mode(data, mode='abs'):
-    if mode.startswith('phs'): data = np.angle(data.filled(0))
-    elif mode.startswith('lin'):
-        data = np.ma.absolute(data.filled(0))
-        data = np.ma.masked_less_equal(data, 0)
-    elif mode.startswith('real'): data = data.real
-    elif mode.startswith('imag'): data = data.imag
-    elif mode.startswith('log'):
-        data = np.ma.absolute(data.filled(0))
-        data = np.ma.masked_less_equal(data, 0)
-        data = np.ma.log10(data)
-    else: raise ValueError('Unrecognized plot mode.')
-    return data
+    rv = [map(int,s.split('_')) for s in chanstr.split(',')]
+    rv = [np.arange(c[0], c[1]+1) if len(c) == 2 else c for c in rv]
+    return np.concatenate(rv)
 
 opts, args = o.parse_args(sys.argv[1:])
 
@@ -112,10 +80,11 @@ if not opts.xlim is None:
 if not opts.ylim is None:
     opts.ylim = map(float, opts.ylim.split('_'))
 is_chan_range, is_time_range = True, True
-if opts.plot_each == 'chan': is_chan_range = False
-elif opts.plot_each == 'time': is_time_range = False
+if opts.plot_each == 'chan':
+    is_chan_range = False
+elif opts.plot_each == 'time':
+    is_time_range = False
 #time_sel = gen_times(opts.time, uv, opts.time_axis, opts.decimate)
-#inttime = uv['inttime'] * opts.decimate
 
 # Loop through UV files collecting relevant data
 plot_f = {}
@@ -125,7 +94,6 @@ plot_t = {'jd':[], 'lst':[]}
 plots = {}
 plt_data = {}
 
-bls = parse_ants(opts.ant)
 data, flgs = [], []
 
 intcnt = 0
@@ -140,13 +108,29 @@ for filecnt, uvfile in enumerate(args):
                 continue
     else:
         uvf = hera_cal.io.HERAData(uvfile, filetype=filetype)
-    dat, flg, _ = uvf.read(bls)
-    data.append(dat); flgs.append(flg) 
     meta = uvf.get_metadata_dict()
-    plot_f['freq'] = meta['freqs'].flatten()
-    plot_f['chan'] = np.arange(meta['freqs'].size)
+    print('    ANTS:', meta['ants'])
+    print('    POLS:', meta['pols'])
+    print('    FREQ RANGE [MHz]:', [meta['freqs'][0]/1e6, meta['freqs'][-1]/1e6])
+    print('    TIME RANGE [JD]:', [meta['times'][0], meta['times'][-1]])
+    print('    LST RANGE [rad]:', [meta['lsts'][0], meta['lsts'][-1]])
+    if opts.ant == 'auto':
+        bls = [(i,i,p) for i in meta['ants'] for p in meta['pols'] if p[0] == p[-1]]
+    else:
+        bls = parse_ants(opts.ant)
+    #import IPython; IPython.embed()
     plot_t['lst'].append(meta['lsts'])
     plot_t['jd'].append(meta['times'])
+    if opts.chan == 'all':
+        chan = np.arange(meta['freqs'].size)
+    else:
+        chan = parse_range(opts.chan)
+    plot_f['freq'] = meta['freqs'].flatten().take(chan)
+    plot_f['chan'] = chan
+    dat, flg, _ = uvf.read(bls, freq_chans=chan)
+    data.append(dat); flgs.append(flg) 
+
+# Concatenate the data from all the files
 if len(data) > 1:
     data = data[0].concatenate(data[1:])
     flgs = flgs[0].concatenate(flgs[1:])
@@ -155,12 +139,17 @@ else:
     data = data[0]
     flgs = flgs[0]
     plot_t = {k:v[0] for k,v in plot_t.items()}
-plot_t['cnt'] = np.arange(plot_t['jd'].size)
+if opts.time == 'all':
+    ints = np.arange(plot_t['jd'].size)
+else:
+    ints = parse_range(opts.time)
+plot_t = {k:v.take(ints) for k,v in plot_t.items()}
+plot_t['cnt'] = ints
 
 def sort_func(a, b):
-    ai,aj,pa = map(int, a.split(','))
-    bi,bj,pb = map(int, b.split(','))
-    if bi > ai or (bi == ai and bj > aj) or (bi == ai and bj == aj and pb < pa): return -1
+    ai,aj,pa = a
+    bi,bj,pb = b
+    if bi > ai or (bi == ai and bj > aj) or (bi == ai and bj == aj and pb > pa): return -1
     return 1
 
 #import IPython; IPython.embed()
@@ -171,6 +160,7 @@ if len(bls) == 0:
     sys.exit(0)
 m2 = int(np.sqrt(len(bls)))
 m1 = int(np.ceil(float(len(bls)) / m2))
+share = opts.share and not (is_chan_range and is_time_range) # disallow shared waterfalls
 
 # Generate all the plots
 dmin,dmax = None, None
@@ -181,7 +171,7 @@ for cnt, bl in enumerate(bls):
         d = np.where(f, np.nan, d)
     plt_data[cnt+1] = d
     d = uvtools.plot.data_mode(d, opts.mode)
-    if not opts.share:
+    if not share:
         plt.subplot(m2, m1, cnt+1)
         dmin,dmax = None,None
         label = ''
@@ -225,7 +215,7 @@ for cnt, bl in enumerate(bls):
     elif not is_chan_range and is_time_range:
         plot_times = plot_t[opts.time_axis]
         xlabel = YLABELS[opts.time_axis] # Y/X mismatch on purpose
-        if opts.chan_axis == 'cnt':
+        if opts.freq_axis == 'cnt':
             chans = plot_f['chan']
             label += '#%d'
         else:
@@ -241,7 +231,7 @@ for cnt, bl in enumerate(bls):
 
     else: raise ValueError('Either time or chan needs to be a range.')
 
-    if not opts.share:
+    if not share:
         title = str(bl)
         plt.title(title)
 if not opts.nolegend and (not is_time_range or not is_chan_range):
@@ -256,7 +246,7 @@ else:
             mode = raw_input('Enter new mode: ')
             for k in plots:
                 try:
-                    d = data_mode(plt_data[k], mode)
+                    d = uvtools.plot.data_mode(plt_data[k], mode)
                     plots[k].set_data(d)
                 except(ValueError):
                     print('Unrecognized plot mode')
