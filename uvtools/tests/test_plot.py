ccd6a0ea599cf232aef7b735310737eaf8f82d86
Aaron Parsons <aparsons@berkeley.edu>
2019-06-21 15:32:08 -0700

    Added a unittest for waterfall

--- uvtools/tests/test_plot.py
+++ uvtools/tests/test_plot.py
@@ -1,9 +1,9 @@
 import unittest
 import uvtools as uvt
 import numpy as np
-import glob
-import os
-import sys
+
+BACKEND = 'Agg'
+#BACKEND = 'MacOSX'
 
 class TestMethods(unittest.TestCase):
     def test_data_mode(self):
@@ -19,6 +19,15 @@ class TestMethods(unittest.TestCase):
         d = uvt.plot.data_mode(data, mode='imag')
         self.assertTrue(np.all(d == -1))
         self.assertRaises(ValueError, uvt.plot.data_mode, data, mode='')
+    def test_waterfall(self):
+        import matplotlib
+        matplotlib.use(BACKEND)
+        data = np.ones((10,10)) - 1j*np.ones((10,10))
+        for mode in ('abs','log','phs','real','imag'):
+            uvt.plot.waterfall(data, mode=mode)
+            matplotlib.pyplot.show()
+            matplotlib.pyplot.clf()
+        
     
 if __name__ == '__main__':
     unittest.main()
