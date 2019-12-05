8184cf97085a2d53a89f2f1fdaf353f7eb03036f
Bobby Pascua <r.pascua@berkeley.edu>
2019-12-04 21:26:54 -0800

    Filled out code for testing plot_diff_waterfall. The test could be a bit more stringent (i.e. testing that the correct labels are made), but that would require quite a bit of extra work.

--- uvtools/tests/test_plot.py
+++ uvtools/tests/test_plot.py
@@ -1,8 +1,8 @@
-import matplotlib
 import matplotlib.pyplot as plt
 import unittest
 import uvtools as uvt
 import numpy as np
+from itertools import combinations
 
 
 def axes_contains(ax, obj_list):
@@ -70,6 +70,7 @@ class TestMethods(unittest.TestCase):
 class TestDiffPlotters(unittest.TestCase):
 
     def setUp(self):
+
         # mock up some UVData objects
         pass
 
@@ -84,8 +85,55 @@ class TestDiffPlotters(unittest.TestCase):
         pass
 
     def test_plot_diff_waterfall(self):
-        # test all different plotting modes
-        pass
+        plot_types = ("time_vs_freq", "time_vs_dly", 
+                      "fr_vs_freq", "fr_vs_dly")
+        # get all combinations
+        plot_types = [list(combinations(plot_types, r)) 
+                      for r in range(len(plot_types))]
+
+        # unpack the nested list
+        plot_types = [item for items in plot_types for item in items]
+
+        # loop over all combinations of plot types, check that the 
+        # right number of subplots are made, noting how many different 
+        # differences are taken and how many plot types there are
+        # also account for colorbars techincally being subplots
+        for plot_type in plot_types:
+            # each plot_type is a list; 3 differences
+            Nplots = 3 * len(plot_type)
+            # each plot consists of an image and a colorbar
+            # so we need to count the colorbars as well
+            Nsubplots = 2 * Nplots
+
+            # make the list of objects to search for
+            elements = [(plt.Subplot, Nsubplots),]
+            
+            # actually make the plot
+            fig = uvt.plot.plot_diff_waterfall(self.uvd1, self.uvd2, 
+                                               self.antpairpol)
+            
+            # check that the correct number of subplots are made
+            self.assertTrue(axes_contains(fig, elements))
+
+            Nimages = 0
+            Ncbars = 0
+            for ax in fig.axes:
+                # check that each Axes object contains either an 
+                # AxesImage (from imshow) or a QuadMesh (from colorbar)
+                image = [(matplotlib.image.AxesImage, 1),]
+                cbar = [(matplotlib.collections.QuadMesh, 1),]
+                contains_image = axes_contain(ax, image)
+                contains_cbar = axes_contain(ax, cbar)
+                Nimages += int(contains_image)
+                Ncbars += int(contains_cbar)
+                self.assertTrue(contains_image or contains_cbar)
+            
+            # check that the amount of colorbars and images is correct
+            self.assertTrue(Nimages == Nplots)
+            self.assertTrue(Ncbars == Nplots)
+
+            # now close the figure
+            plt.close(fig)
 
     def test_bad_metadata(self):
         # test for freq arrays not matching
