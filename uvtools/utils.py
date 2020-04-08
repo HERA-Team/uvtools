4d17a46f6d638f85795b38f205f64e7c70c28266
Bobby Pascua <r.pascua@berkeley.edu>
2020-04-07 20:17:32 -0700

    Make logic for checking time/frequency array sizes clearer.

--- uvtools/utils.py
+++ uvtools/utils.py
@@ -182,7 +182,7 @@ def check_uvd_pair_metadata(uvd1, uvd2):
 
     t1vals = np.unique(uvd1.time_array)
     t2vals = np.unique(uvd2.time_array)
-    if not (uvd1.Ntimes == 1 or uvd2.Ntimes == 1):
+    if uvd1.Ntimes > 1 and uvd2.Ntimes > 1:
         if not np.all(
             np.isclose(t1vals, t2vals, rtol=0, atol=dx(t1vals, t2vals))
         ):
@@ -192,7 +192,7 @@ def check_uvd_pair_metadata(uvd1, uvd2):
 
     f1vals = uvd1.freq_array[0]
     f2vals = uvd2.freq_array[0]
-    if not (uvd1.Nfreqs == 1 or uvd2.Nfreqs == 1):
+    if uvd1.Nfreqs > 1 and uvd2.Nfreqs > 1:
         if not np.all(np.isclose(f1vals, f2vals, atol=dx(f1vals, f2vals))):
             raise MetadataError(
                 "Frequency values disagree more than the mean channel width."
