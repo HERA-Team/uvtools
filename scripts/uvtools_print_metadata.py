4606a1854d57453c9384862568291aad73c7d63c
Bryna Hazelton <bryna.hazelton@gmail.com>
2021-10-15 10:37:32 -0700

    Only print the antenna numbers that have data associated with them

--- scripts/uvtools_print_metadata.py
+++ scripts/uvtools_print_metadata.py
@@ -3,16 +3,17 @@
 # Copyright (c) 2021 HERA collaboration
 # Licensed under the MIT License
 
-"""Print all the antennas in a UVData readable file (e.g. uhv5)."""
+"""Print all the antennas with data in a UVData readable file (e.g. uhv5)."""
 
 import argparse
 
+import numpy as np
 from pyuvdata import UVData
 
 # setup argparse
 a = argparse.ArgumentParser(
-    description="A command-line script for printing all the antennas in a UVData "
-    "readable file"
+    description="A command-line script for printing all the antennas with visibilities "
+    "associated with them in a UVData readable file."
 )
 a.add_argument(
     "file",
@@ -28,4 +29,4 @@ uvd = UVData()
 # read in the metadata only
 uvd.read(args.file, read_data=False)
 
-print(uvd.antenna_numbers)
+print(np.union1d(uvd.ant_1_array, uvd.ant_2_array))
